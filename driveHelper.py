5bfa82ddc6dccdbb2efa022f224720923674a694
Lee kwang ho <lex.less38@gmail.com>
2020-05-17 01:55:11 +0900

    Add files via upload

--- driveHelper.py
+++ driveHelper.py
@@ -64,6 +64,9 @@ class GHelper:
 
         return file.get('id')
 
+    def delete(self, file_id):
+        self.service.files().delete(fileId=file_id).execute()
+
     def upload(self, file_name, file_path='./', folder_id=[], as_docs=False, verbose=False):
         # parents 는 리스트 형태로 받아야함
         if isinstance(folder_id, str):
@@ -80,6 +83,7 @@ class GHelper:
             print('File ID: %s' % file.get('id'))
         
         return file.get('id')
+
 
     def download(self, file_id, file_path='./', verbose=False):
         file_name = self.service.files().get(fileId=file_id).execute()['name']
@@ -102,3 +106,101 @@ class GHelper:
         file = self.service.files().get(fileId=file_id).execute()
         print("name : {}".format(file['name']))
         print("MIME Type : {}".format(file['mimeType']))
+
+
+
+def preprocess_pngs(data_path, save_path, verbose=False):
+    png_list = glob.glob(data_path + '*.png')
+    thr = 200
+    try:
+        os.mkdir(save_path)
+    except FileExistsError:
+        pass
+    for file_path in png_list:
+        img = cv2.imread(file_path,0)
+        img = img[230:850,335:1400]
+        ret,thresh2 = cv2.threshold(img,thr,255,cv2.THRESH_BINARY_INV)
+        png_name = file_path.split('\\')[-1]
+        save_name = save_path + png_name[:-4] + "_BINARY_INV.png"
+        if verbose:
+            print("FROM : {}".format(file_path))
+            print("TO : {}".format(save_name))
+        cv2.imwrite(save_name, thresh2)
+
+def seperate_rows(data_path, save_path, verbose=False):
+    png_list = glob.glob(data_path + '*.png')
+    try:
+        os.mkdir(save_path)
+    except FileExistsError:
+        pass
+    for file_path in png_list:
+        img = cv2.imread(file_path,0)
+        height, width = img.shape
+        
+        # height sep
+        for x in range(1,6):
+            line_idx = int(x * height/6)
+            img[line_idx] = [1]*width
+        
+        png_name = file_path.split('\\')[-1]
+        save_name = save_path + png_name[:-4] + "_sep.png"
+        if verbose:
+            print("FROM : {}".format(file_path))
+            print("TO : {}".format(save_name))
+        cv2.imwrite(save_name_name, img)
+
+def seperate_pngs(data_path, save_path, verbose=False):
+    png_list = glob.glob(data_path + '*.png')
+    try:
+        os.mkdir(save_path)
+    except FileExistsError:
+        pass
+    for file_path in png_list:
+        img = cv2.imread(file_path,0)
+        height, width = img.shape
+        
+        # height sep
+        for x in range(1,6):
+            line_idx = int(x * height/6)
+            img[line_idx] = [1]*width
+            
+        # width sep
+        img_name = img[:,:width//2]
+        img_power = img[:,width//2:]
+        
+        png_name = file_path.split('\\')[-1]
+        save_name_name = save_path + png_name[:-4] + "_name.png"
+        save_name_power= save_path + png_name[:-4] + "_power.png"
+        if verbose:
+            print("FROM : {}".format(file_path))
+            print("TO : {}".format(save_name_name))
+            print("TO : {}".format(save_name_power))
+        cv2.imwrite(save_name_name, img_name)
+        cv2.imwrite(save_name_power, img_power)
+
+
+def seperate1x1(data_path, save_path, verbose=False):
+    png_list = glob.glob(data_path + '*.png')
+    try:
+        os.mkdir(save_path)
+    except FileExistsError:
+        pass
+    id_num = 0
+    for file_path in png_list:
+        img = cv2.imread(file_path, 0)
+        height, width = img.shape
+
+        png_name = file_path.split('\\')[-1]
+        save_name = save_path + png_name[:-4]
+
+        # height sep
+        for x in range(6):
+            fidx = int(x * height/6)
+            eidx = int((x+1) * height/6)
+            plt.imshow(img[fidx:eidx, :], 'gray')
+
+            if verbose:
+                print("FROM : {}".format(file_path))
+                print("TO : {}".format(save_name + "_{}.png".format(id_num)))
+            cv2.imwrite(save_name + "_{}.png".format(id_num), img[fidx:eidx, :])
+            id_num += 1
